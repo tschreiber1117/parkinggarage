943fa66eb48a8898f1348de02a9539e8f07b91c2
Todd Schreiber <todd.schreiber@gmail.com>
2021-06-26 19:56:45 -0500

    first

---
+++ parkinggarage.py
@@ -0,0 +1,15 @@
+class parking_garage:
+
+
+    def __init__(self, tickets = ['ticket','ticket','ticket'], parking_spaces = ['spaces','spaces','spaces'], current_ticket = {'paid':True}):
+        self.tickets = tickets
+        self.parking_spaces = parking_spaces
+        self.current_ticket = current_ticket
+
+    def take_ticket():
+        tickets.remove('ticket')
+        parking_spaces.remove('spaces')
+
+
+
+
